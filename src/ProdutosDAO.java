75137f58b9e0b0dd3351ded38f665c9d80d48e0d
gustavokato7-bit <gustavokato7-bit@exemplo.com>
2026-01-19 11:09:19 -0300

    Implementada funcionalidade de salvar produtos no banco de dados com mensagem de sucesso

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -23,13 +23,25 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+   String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+
+    try {
+        conn = new conectaDAO().connectDB();
+        prep = conn.prepareStatement(sql);
+
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+
+        prep.execute();
+        prep.close();
+
+        JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + e.getMessage());
     }
-    
+    }
     public ArrayList<ProdutosDTO> listarProdutos(){
         
         return listagem;
