af66047a7557c468dcac8660e6e4960212b03caa
gustavokato7-bit <gustavokato7-bit@exemplo.com>
2026-01-19 11:30:09 -0300

    Implementação listagem de produtos cadastrados na tela de listagem

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -42,13 +42,31 @@ public class ProdutosDAO {
         JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + e.getMessage());
     }
     }
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
+public ArrayList<ProdutosDTO> listarProdutos() {
+
+    ArrayList<ProdutosDTO> lista = new ArrayList<>();
+    String sql = "SELECT * FROM produtos";
+
+    try {
+        conn = new conectaDAO().connectDB();
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+
+        while (resultset.next()) {
+            ProdutosDTO produto = new ProdutosDTO();
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+
+            lista.add(produto);
+        }
+
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + e.getMessage());
     }
-    
-    
-    
-        
+
+    return lista;
+}
 }
 
